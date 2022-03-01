71cb832ab6d68e8636c72c552d47ed7a389333b6
zeenayouhan <zinayouhan33@gmail.com>
2022-03-01 09:38:46 +0530

    initial

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      child: MaterialApp(
+        title: "Great Places",
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+          accentColor: Colors.amber,
+        ),
+        home: ,
+      ),
+    );
+  }
+}
